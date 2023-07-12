1eb37949b46108650863c93aa515346a26696a05
Eterance-PC <eterance@sina.com>
2023-07-12 19:01:06 +0800

    参数绑定

--- ImageAutoResizer/ViewModels/CoreViewModel.cs
+++ ImageAutoResizer/ViewModels/CoreViewModel.cs
@@ -11,6 +11,12 @@ using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Formats.Tga;
 using SixLabors.ImageSharp.Formats.Tiff;
+using Prism.Commands;
+using System.Windows.Input;
+using Prism.Services.Dialogs;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using System.Linq;
 
 namespace ImageBatchResizer.ViewModels
 {
@@ -22,20 +28,73 @@ namespace ImageBatchResizer.ViewModels
         private ResizeModeModel _selectedResizeModeModel;
         private bool _isEnableInputResUpperLimit = false;
         private bool _isEnableInputResLowerLimit = true;
-        private bool _isResLimitationAdaptPortraitImage = true;
+        private bool _isInputResLimitationAdaptPortraitImage = false;
         private bool _isAcceptBmp = true;
         private bool _isAcceptJpeg = true;
         private bool _isAcceptTga = true;
         private bool _isAcceptPng = true;
         private bool _isAcceptTiff = true;
         private bool _isAcceptWebP = true;
+        private bool _isFileSizeFirstMode = true;
+        private double _targetSizeLowerLimit = 300;
+        private double _targetSizeUpperLimit = 350;
+        private int _binarySearchTimesLimit = 20;
+        private bool _isDeleteSmallerThanTarget = false;
+        private int _targetResolutionWidthLowerLimit = 1920;
+        private int _targetResolutionHeightLowerLimit = 1200;
+        private bool _isEnableResLowerLimit = false;
+        private bool _isOutputResLimitationAdaptPortraitImage = false;
+        private bool _fileSizeFirst_DoNothing = true;
+        private bool _fileSizeFirst_Delete = false;
+        private bool _fileSizeFirst_IgnoreSizeLimit = false;
+        private bool _isEnableTargetSizeLimit = false;
+        private bool _resFirst_DoNothing = true;
+        private bool _resFirst_Delete = false;
+        private bool _resFirst_IgnoreSizeLimit = false;
+        private int _quality = 90;
+        private string _outputPath = "";
+        private string _outputFileNamePattern = "{name}";
+        private int _InputResUpperLimitWidth = 10000;
+        private int _InputResUpperLimitHeight = 8000;
+        private int _InputResLowerLimitWidth = 1920;
+        private int _InputResLowerLimitHeight = 1200;
+        private bool _isEnableOriginFileName = true;
+        private bool _isEnableIndex = false;
+        private bool _isEnableTime;
+        private bool _isEnableQuality;
 
+        public bool IsEnableParametersPanel
+        {
+            get => _isEnableParametersPanel;
+            set
+            {
+                SetValue(ref _isEnableParametersPanel, value);
+            }
+        }
+
+        #region 输入参数
         public bool IsEnableInputResUpperLimit
         {
             get => _isEnableInputResUpperLimit;
             set
             {
                 SetValue(ref _isEnableInputResUpperLimit, value);
+            }
+        }
+        public int InputResUpperLimitWidth
+        {
+            get => _InputResUpperLimitWidth;
+            set
+            {
+                SetValue(ref _InputResUpperLimitWidth, value);
+            }
+        }
+        public int InputResUpperLimitHeight
+        {
+            get => _InputResUpperLimitHeight;
+            set
+            {
+                SetValue(ref _InputResUpperLimitHeight, value);
             }
         }
 
@@ -47,13 +106,29 @@ namespace ImageBatchResizer.ViewModels
                 SetValue(ref _isEnableInputResLowerLimit, value);
             }
         }
-
-        public bool IsResLimitationAdaptPortraitImage
+        public int InputResLowerLimitWidth
         {
-            get => _isResLimitationAdaptPortraitImage;
+            get => _InputResLowerLimitWidth;
             set
             {
-                SetValue(ref _isResLimitationAdaptPortraitImage, value);
+                SetValue(ref _InputResLowerLimitWidth, value);
+            }
+        }
+        public int InputResLowerLimitHeight
+        {
+            get => _InputResLowerLimitHeight;
+            set
+            {
+                SetValue(ref _InputResLowerLimitHeight, value);
+            }
+        }
+
+        public bool IsInputResLimitationAdaptPortraitImage
+        {
+            get => _isInputResLimitationAdaptPortraitImage;
+            set
+            {
+                SetValue(ref _isInputResLimitationAdaptPortraitImage, value);
             }
         }
 
@@ -105,16 +180,150 @@ namespace ImageBatchResizer.ViewModels
                 SetValue(ref _isAcceptWebP, value);
             }
         }
+        #endregion
 
-        public bool IsEnableParametersPanel
+        public bool IsFileSizeFirstMode
         {
-            get => _isEnableParametersPanel;
+            get => _isFileSizeFirstMode;
             set
             {
-                SetValue(ref _isEnableParametersPanel, value);
+                SetValue(ref _isFileSizeFirstMode, value);
             }
         }
 
+        #region 输出参数
+        public double TargetSizeLowerLimit
+        {
+            get => _targetSizeLowerLimit;
+            set
+            {
+                SetValue(ref _targetSizeLowerLimit, value);
+            }
+        }
+        public double TargetSizeUpperLimit
+        {
+            get => _targetSizeUpperLimit;
+            set
+            {
+                SetValue(ref _targetSizeUpperLimit, value);
+            }
+        }
+        public int BinarySearchTimesLimit
+        {
+            get => _binarySearchTimesLimit;
+            set
+            {
+                SetValue(ref _binarySearchTimesLimit, value);
+            }
+        }
+        public bool IsDeleteSmallerThanTarget
+        {
+            get => _isDeleteSmallerThanTarget;
+            set
+            {
+                SetValue(ref _isDeleteSmallerThanTarget, value);
+            }
+        }
+        public bool IsEnableResLowerLimit
+        {
+            get => _isEnableResLowerLimit;
+            set
+            {
+                SetValue(ref _isEnableResLowerLimit, value);
+            }
+        }
+
+        public int TargetResolutionWidthLowerLimit
+        {
+            get => _targetResolutionWidthLowerLimit;
+            set
+            {
+                SetValue(ref _targetResolutionWidthLowerLimit, value);
+            }
+        }
+        public int TargetResolutionHeightLowerLimit
+        {
+            get => _targetResolutionHeightLowerLimit;
+            set
+            {
+                SetValue(ref _targetResolutionHeightLowerLimit, value);
+            }
+        }
+        public bool IsOutputResLimitationAdaptPortraitImage
+        {
+            get => _isOutputResLimitationAdaptPortraitImage;
+            set
+            {
+                SetValue(ref _isOutputResLimitationAdaptPortraitImage, value);
+            }
+        }
+        public bool FileSizeFirst_DoNothing
+        {
+            get => _fileSizeFirst_DoNothing;
+            set
+            {
+                SetValue(ref _fileSizeFirst_DoNothing, value);
+            }
+        }
+        public bool FileSizeFirst_Delete
+        {
+            get => _fileSizeFirst_Delete;
+            set
+            {
+                SetValue(ref _fileSizeFirst_Delete, value);
+            }
+        }
+        public bool FileSizeFirst_IgnoreSizeLimit
+        {
+            get => _fileSizeFirst_IgnoreSizeLimit;
+            set
+            {
+                SetValue(ref _fileSizeFirst_IgnoreSizeLimit, value);
+            }
+        }
+        public bool IsEnableTargetSizeLimit
+        {
+            get => _isEnableTargetSizeLimit;
+            set
+            {
+                SetValue(ref _isEnableTargetSizeLimit, value);
+            }
+        }
+        public bool ResFirst_DoNothing
+        {
+            get => _resFirst_DoNothing;
+            set
+            {
+                SetValue(ref _resFirst_DoNothing, value);
+            }
+        }
+        public bool ResFirst_Delete
+        {
+            get => _resFirst_Delete;
+            set
+            {
+                SetValue(ref _resFirst_Delete, value);
+            }
+        }
+        public bool ResFirst_IgnoreSizeLimit
+        {
+            get => _resFirst_IgnoreSizeLimit;
+            set
+            {
+                SetValue(ref _resFirst_IgnoreSizeLimit, value);
+            }
+        }
+        #endregion
+
+        #region 通用输出参数
+        public int Quality
+        {
+            get => _quality;
+            set
+            {
+                SetValue(ref _quality, value);
+            }
+        }
         public ObservableCollection<FormatModel> FormatList { get; set; }
 
         public FormatModel SelectedFormatModel
@@ -196,11 +405,95 @@ namespace ImageBatchResizer.ViewModels
             ResizeModeList.Add(new("Stretch", ResizeMode.Stretch));
         }
 
+        public string OutputPath
+        {
+            get => _outputPath;
+            set
+            {
+                SetValue(ref _outputPath, value);
+            }
+        }
+        public ICommand SelectOutputFolderCommand { get; private set; }
+        public string OutputFileNamePattern
+        {
+            get => _outputFileNamePattern;
+            set
+            {
+                SetValue(ref _outputFileNamePattern, value);
+            }
+        }
+        private string GetDash()
+        {
+            if (OutputFileNamePattern != "")
+            {
+                return "-";
+            }
+            return "";
+        }
+        private void ProcessOutputFileNamePattern(bool value, string PlaceHolder)
+        {
+            if (value)
+            {
+                OutputFileNamePattern = @$"{OutputFileNamePattern}{GetDash()}{PlaceHolder}";
+            }
+            else
+            {
+                OutputFileNamePattern = OutputFileNamePattern.Replace(@$"-{PlaceHolder}", "");
+                OutputFileNamePattern = OutputFileNamePattern.Replace(PlaceHolder, "");
+                OutputFileNamePattern = OutputFileNamePattern.Trim('-');
+                OutputFileNamePattern = OutputFileNamePattern.Trim();
+            }
+        }
+        public bool IsEnableOriginFileName
+        {
+            get => _isEnableOriginFileName;
+            set
+            {
+                ProcessOutputFileNamePattern(value, "{name}");
+                SetValue(ref _isEnableOriginFileName, value);
+            }
+        }
+        public bool IsEnableIndex
+        {
+            get => _isEnableIndex;
+            set
+            {
+                ProcessOutputFileNamePattern(value, "{index}");
+                SetValue(ref _isEnableIndex, value);
+            }
+        }
+        public bool IsEnableTime
+        {
+            get => _isEnableTime;
+            set
+            {
+                ProcessOutputFileNamePattern(value, "{time}");
+                SetValue(ref _isEnableTime, value);
+            }
+        }
+        public bool IsEnableQuality
+        {
+            get => _isEnableQuality;
+            set
+            {
+                ProcessOutputFileNamePattern(value, "{quality}");
+                SetValue(ref _isEnableQuality, value);
+            }
+        }
+
+        #endregion
+
         public CoreViewModel()
         {
             InitFormatList();
             InitResamplerList();
             InitResizeModeList();
+            SelectOutputFolderCommand = new DelegateCommand(() => 
+            {
+                var dlg = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+                var result = dlg.ShowDialog();
+                if (result == true) OutputPath = dlg.SelectedPath;
+            });
         }
 
         private void Resize()
